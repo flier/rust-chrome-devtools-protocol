1a61ac33bf28e7e4db713c663583ba65812bdbac
Flier Lu <flier.lu@gmail.com>
2019-08-05 18:54:21 +0800

    handle deprecated and experimental feature

--- build.rs
+++ build.rs
@@ -9,6 +9,8 @@ use failure::{bail, err_msg, Error};
 use indented::indented;
 
 fn gen<P: AsRef<Path>>(path: P) -> Result<(), Error> {
+    println!("cargo:rerun-if-changed={:?}", path.as_ref());
+
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir)
         .join(path.as_ref().file_name().unwrap())
@@ -28,7 +30,7 @@ fn gen<P: AsRef<Path>>(path: P) -> Result<(), Error> {
 
     writeln!(
         f,
-        "{}pub const VERSION: &str = \"{}.{}\";\n",
+        "{}pub const PROTOCOL_VERSION: &str = \"{}.{}\";\n",
         Comments(&pdl.description),
         pdl.version.major,
         pdl.version.minor
@@ -37,10 +39,20 @@ fn gen<P: AsRef<Path>>(path: P) -> Result<(), Error> {
     for domain in pdl.domains {
         writeln!(
             f,
-            r#"{}pub mod {} {{
+            r#"{}{}{}pub mod {} {{
 {}}}
 "#,
             Comments(&domain.description),
+            if domain.experimental {
+                "#[cfg(feature = \"experimental\")]\n"
+            } else {
+                ""
+            },
+            if domain.deprecated {
+                "#[deprecated]\n"
+            } else {
+                ""
+            },
             domain.name,
             indented(Mod(&domain))
         )?;
@@ -67,7 +79,7 @@ impl<'a> fmt::Display for Mod<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         writeln!(
             f,
-            r#"#![allow(non_snake_case, unused_imports)]
+            r#"#![allow(deprecated, non_snake_case, unused_imports)]
 
 use serde::{{Serialize, Deserialize}};
 
@@ -80,10 +92,25 @@ use crate::*;
 
             match ty.item {
                 Some(pdl::Item::Enum(ref variants)) => {
-                    writeln!(f, "{}", Enum(ty.id, variants))?;
+                    writeln!(
+                        f,
+                        "{}{}",
+                        if ty.deprecated { "#[deprecated]\n" } else { "" },
+                        Enum(ty.id, variants)
+                    )?;
                 }
                 Some(pdl::Item::Properties(ref props)) => {
-                    writeln!(f, "{}", Struct(ty.id, props))?;
+                    writeln!(
+                        f,
+                        "{}{}{}",
+                        if ty.experimental {
+                            "#[cfg(feature = \"experimental\")]\n"
+                        } else {
+                            ""
+                        },
+                        if ty.deprecated { "#[deprecated]\n" } else { "" },
+                        Struct(ty.id, props)
+                    )?;
                 }
                 None => {
                     writeln!(f, "pub type {} = {};\n", ty.id, Type(&ty.extends, ty.id))?;
@@ -92,45 +119,86 @@ use crate::*;
         }
 
         for cmd in &self.0.commands {
-            if !cmd.parameters.is_empty() {
-                writeln!(
-                    f,
-                    "/// Request parameters to the `{}::{}` command.\n{}",
-                    self.0.name,
-                    cmd.name,
-                    Struct(
-                        &format!("{}Request", cmd.name.to_capitalized()),
-                        &cmd.parameters
-                    )
-                )?;
-            }
-            if !cmd.returns.is_empty() {
-                writeln!(
-                    f,
-                    "/// Response returns from the `{}::{}` command.\n{}",
-                    self.0.name,
-                    cmd.name,
-                    Struct(
-                        &format!("{}Response", cmd.name.to_capitalized()),
-                        &cmd.returns
-                    )
-                )?;
-            }
+            writeln!(
+                f,
+                "/// The `{}::{}` command.\n{}{}pub type {} = {4}Request;\n",
+                self.0.name,
+                cmd.name,
+                if cmd.experimental {
+                    "#[cfg(feature = \"experimental\")]\n"
+                } else {
+                    ""
+                },
+                if cmd.deprecated {
+                    "#[deprecated]\n"
+                } else {
+                    ""
+                },
+                cmd.name.to_capitalized(),
+            )?;
+            writeln!(
+                f,
+                "/// Request parameters to the `{}::{}` command.\n{}{}{}",
+                self.0.name,
+                cmd.name,
+                if cmd.experimental {
+                    "#[cfg(feature = \"experimental\")]\n"
+                } else {
+                    ""
+                },
+                if cmd.deprecated {
+                    "#[deprecated]\n"
+                } else {
+                    ""
+                },
+                Struct(
+                    &format!("{}Request", cmd.name.to_capitalized()),
+                    &cmd.parameters
+                )
+            )?;
+            writeln!(
+                f,
+                "/// Response returns from the `{}::{}` command.\n{}{}{}",
+                self.0.name,
+                cmd.name,
+                if cmd.experimental {
+                    "#[cfg(feature = \"experimental\")]\n"
+                } else {
+                    ""
+                },
+                if cmd.deprecated {
+                    "#[deprecated]\n"
+                } else {
+                    ""
+                },
+                Struct(
+                    &format!("{}Response", cmd.name.to_capitalized()),
+                    &cmd.returns
+                )
+            )?;
         }
 
         for evt in &self.0.events {
-            if !evt.parameters.is_empty() {
-                writeln!(
-                    f,
-                    "/// Event parameters for the `{}::{}` event.\n{}",
-                    self.0.name,
-                    evt.name,
-                    Struct(
-                        &format!("{}Event", evt.name.to_capitalized()),
-                        &evt.parameters
-                    )
-                )?;
-            }
+            writeln!(
+                f,
+                "/// Event parameters for the `{}::{}` event.\n{}{}{}",
+                self.0.name,
+                evt.name,
+                if evt.experimental {
+                    "#[cfg(feature = \"experimental\")]\n"
+                } else {
+                    ""
+                },
+                if evt.deprecated {
+                    "#[deprecated]\n"
+                } else {
+                    ""
+                },
+                Struct(
+                    &format!("{}Event", evt.name.to_capitalized()),
+                    &evt.parameters
+                )
+            )?;
         }
 
         Ok(())
@@ -169,6 +237,7 @@ impl<'a> fmt::Display for Enum<'a> {
         writeln!(
             f,
             r#"#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
 pub enum {} {{"#,
             self.0,
         )?;
@@ -199,6 +268,7 @@ impl<'a> fmt::Display for Struct<'a> {
         writeln!(
             f,
             r#"#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct {} {{"#,
             self.0
         )?;
@@ -213,9 +283,14 @@ pub struct {} {{"#,
             if let pdl::Type::Enum(ref variants) = prop.ty {
                 write!(
                     f,
-                    "\n/// Allow values for the `{}::{}` field.\n{}",
+                    "\n/// Allow values for the `{}::{}` field.\n{}{}",
                     self.0,
                     prop.name,
+                    if prop.experimental {
+                        "#[cfg(feature = \"experimental\")]\n"
+                    } else {
+                        ""
+                    },
                     Enum(
                         &format!("{}{}", self.0, prop.name.to_capitalized()),
                         variants
@@ -234,8 +309,18 @@ impl<'a> fmt::Display for Field<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{}pub {}: ",
+            "{}{}{}pub {}: ",
             Comments(&self.0.description),
+            if self.0.experimental {
+                "#[cfg(feature = \"experimental\")]\n"
+            } else {
+                ""
+            },
+            if self.0.deprecated {
+                "#[deprecated]\n"
+            } else {
+                ""
+            },
             mangle(self.0.name)
         )?;
 
